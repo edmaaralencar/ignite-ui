04361913ad972dd29344129e8bc63cf6cffec9ad
Edmar Alencar <edmaaralencar1@gmail.com>
2022-10-24 09:26:16 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,43 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nostrum optio voluptatem, ad explicabo qui quibusdam, illo facilis perferendis porro voluptate eum accusamus at provident molestiae nulla quis maxime eligendi.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
